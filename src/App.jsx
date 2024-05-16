02ad262192d2bbeb2f0c4cff681d5ecd9304f35e
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2024-05-16 20:41:10 -0300

    some new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-
 import Header from "./components/Header";
-import "./App.css";
+import Gallery from "./components/Gallery";
+import "./css/App.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [movies, setMovies] = useState([1, 2, 3, 4, 5]);
+  const getMovies = () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: process.env.TMDB_token,
+      },
+    };
+
+    try {
+      const fetchResponse = fetch(
+        "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
+        options
+      );
+      const response = fetchResponse.json();
+      setMovies(response.results);
+      console.log(movies);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(getMovies, []);
   return (
     <>
       <Header></Header>
-      <Gallery></Gallery>
+      <Gallery {...{ movies }}></Gallery>
     </>
   );
 }
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,3 +1,16 @@
-export default function Gallery() {
-  return <></>;
+import Movie from "./Movie";
+export default function Gallery({ movies }) {
+  return (
+    <div class="container">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        {movies.map((film) => {
+          return (
+            <div className="col">
+              <Movie data={film}></Movie>;
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
